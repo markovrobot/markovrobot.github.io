de01d9a1194ae7fce9c8b5706ce78f8fadad86a3
BryanGrisales <bryangrisales01@gmail.com>
2024-12-09 03:09:08 -0500

    .

--- environment.js
+++ environment.js
@@ -1,4 +1,4 @@
-import * as THREE from './node_modules/three/build/three.module.js';
+import * as THREE from 'three';
 
 function getRandomPositionInCell(cell, padding = 1) {
     const { minX, maxX, minZ, maxZ } = cell;
--- utils.js
+++ utils.js
@@ -1,4 +1,4 @@
-import * as THREE from './node_modules/three/build/three.module.js';
+import * as THREE from 'three';
 import { STATES } from './markov.js';
 
 export function checkStateTransitions(state, matrix) {
